0b4c6e09944d7d05de9395c4f0b725672d85d77a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-23 23:34:41 +0530

    Kanban page done

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {GridComponent, ColumnDirective, ColumnsDirective,Page,Search,Inject,Toolbar} 
 from '@syncfusion/ej2-react-grids';
 import {employeesData,employeesGrid} from '../data/dummy';
-import {Header} from '../components'
+import {Header} from '../components';
 
 const Empolyees = () => {
   return (
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {KanbanComponent,ColumnDirective,ColumnsDirective} from '@syncfusion/ej2-react-kanban'
+import {kanbanData,kanbanGrid} from '../data/dummy';
+import {Header} from '../components'
 
 const Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Kanban"/>
+      <KanbanComponent id='kanban' dataSource={kanbanData} cardSettings={{contentField:'Summary', 
+    headerField:'Id'}} keyField='Status'>
+        <ColumnsDirective>
+          {kanbanGrid.map((item,index)=> <ColumnDirective key={index} {...item}/>)}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 
